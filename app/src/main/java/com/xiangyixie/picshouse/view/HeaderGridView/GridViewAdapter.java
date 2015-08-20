a7c0f4d6fa8fb0a4985a369f52d236e2a155d987
xiexiangyi0 <xiexiangyi0@gmail.com>
2015-08-19 23:44:13 -0700

    add GridViewAdapter.java to bind grid_item.xml and image data to grid view in user tab.

---
+++ app/src/main/java/com/xiangyixie/picshouse/view/HeaderGridView/GridViewAdapter.java
@@ -0,0 +1,31 @@
+package com.xiangyixie.picshouse.view.HeaderGridView;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by xiangyixie on 8/19/15.
+ */
+public class GridViewAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
