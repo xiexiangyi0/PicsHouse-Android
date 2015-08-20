ca4ef478ab8b244adad8fa61e4f80caa36703a75
xiexiangyi0 <xiexiangyi0@gmail.com>
2015-08-19 23:52:41 -0700

    implement GridViewAdapter

--- app/src/main/java/com/xiangyixie/picshouse/view/HeaderGridView/GridViewAdapter.java
+++ app/src/main/java/com/xiangyixie/picshouse/view/HeaderGridView/GridViewAdapter.java
@@ -1,8 +1,10 @@
 package com.xiangyixie.picshouse.view.HeaderGridView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 /**
  * Created by xiangyixie on 8/19/15.
@@ -11,21 +13,29 @@ public class GridViewAdapter extends BaseAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return 100;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View v;
+        if(convertView==null) {
+            v = new TextView(convertView.getContext());
+        } else {
+            v = (TextView) convertView;
+        }
+        Log.d("Updating -----", String.valueOf(position));
+
+        return v;
     }
 }
